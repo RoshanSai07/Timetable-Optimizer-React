36754c36b86dd0f973c907b7eb14f3e3f5004647
Roshan <roshankandregula07@gmail.com>
2026-06-12 14:19:03 +0530

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { Dashboard } from "./pages/Dashboard";
+import { Analytics } from "@vercel/analytics/next";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Analytics />
         </Routes>
       </BrowserRouter>
     </>
